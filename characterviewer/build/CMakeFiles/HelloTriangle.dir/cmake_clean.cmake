2b90aad21d5a3331fa06d42dfb87a73e14bf4374
tachibis <chibisova12@gmail.com>
2026-04-12 00:15:06 +0900

    initial opengl setup

---
+++ characterviewer/build/CMakeFiles/HelloTriangle.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloTriangle.dir/glad.c.o"
+  "CMakeFiles/HelloTriangle.dir/glad.c.o.d"
+  "CMakeFiles/HelloTriangle.dir/main.cpp.o"
+  "CMakeFiles/HelloTriangle.dir/main.cpp.o.d"
+  "HelloTriangle"
+  "HelloTriangle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/HelloTriangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
